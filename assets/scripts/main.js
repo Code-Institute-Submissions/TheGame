f0c69796ac53db296c896ad007667befaf5aa180
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-04-09 21:38:28 +0000

    first test of JS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,16 @@
 $( document ).ready(function() {
     
-    $click(function() {
-     
+    $("button").click(function(event) {
+        
+        function handler( event ) {
+        
+            var target = $( event.target );
+            if ( target.is( "top_one" ) ) {
+                $("top_one").toggle(addClass("red"), removeClass("red"));
 
+        }
+    };
+   
+    })
 
-
-});
-})
+}())

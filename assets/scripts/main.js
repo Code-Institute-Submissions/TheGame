c4ba56b3f8b58c0ecca6af5bcd11e57d65d6a882
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-04-11 15:13:09 +0000

    test: functionality of jQuery

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,37 @@
 
    $(document).ready(function(){
 
-        $("#top_two").on("click", function(){
-            $("#top_two").remove("button").addClass("red");
-     
-        })
-        
-  
+        // $("#top_two").on("click", function(){
+        //     $("#top_two").remove("button").toggleClass("red");     
+        // })
 
+        $(".toRed").on("click", function(){
+            $(this).toggleClass("red");     
+        })
+         if("#top_two"+ ".red"==true && "#top_one"+ ".red"==true){
+             $(this).slideUp();
+         }
+        
+          $(".toBlue").on("click", function(){
+            $(this).toggleClass("blue");     
+        })
+
+         $(".toGreen").on("click", function(){
+            $(this).toggleClass("green");     
+        })
+
+         $(".toZonk").on("click", function(){
+            $(this).toggleClass("zonk").innerHTML("nothing");     
+        })
  }) 
 
-   function Game(click){
+//    function Game(click){
         
-        if (".red" == false){
-            addClass("red");
-        }else if (".red" == false){
-            removeClass("red");
-        } else {
-            console.log("Try harder Stan!")
-        }
-    }
+//         if (".red" == false){
+//             addClass("red");
+//         }else if (".red" == false){
+//             removeClass("red");
+//         } else {
+//             console.log("Try harder Stan!")
+//         }
+//     }

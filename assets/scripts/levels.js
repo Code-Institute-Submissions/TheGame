0436a9d5e5eb01fa787f93f1ccb833564823482a
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-04-17 18:48:45 +0000

    fixed: code that was doubled removed, more generic functions

--- assets/scripts/levels.js
+++ assets/scripts/levels.js
@@ -1,44 +1,40 @@
+function level2(success) {
 
-        function level2(success){
-    
 
-        
-        alert("Are you ready for the real challenge?")
-              
-        
-        location.replace("level2.html")           
-        
+	alert("Are you ready for the real challenge?")
 
-  }
- 
-    function level3(success){
-    
 
-        
-        alert("You earned 15 points!!")
-              
-        
-        location.replace("level3.html")
-           
-        
- };
+	location.replace("level2.html")
 
-        
-    function theEnd(success){
-    
 
-        
-        alert(" Congratulations, you scored 39 points and finished the game!! We hope to see you again soon.")
-              
-        
-        location.replace("index.html")
-    };
+}
 
-    function fail(fail) {
-        
-        alert(" Oh no, I hope this will make you happy")
-              
-        
-        location.replace("https://www.youtube.com/watch?v=NmPQNvbXyic")
+function level3(success) {
 
-    };
+
+	alert("You earned 15 points!!")
+
+
+	location.replace("level3.html")
+
+
+};
+
+
+function theEnd(success) {
+
+
+	alert(" Congratulations, you scored 39 points and finished the game!! We hope to see you again soon.")
+
+
+	location.replace("index.html")
+};
+
+function fail(fail) {
+
+	alert(" Oh no, I hope this will make you happy")
+
+
+	location.replace("https://www.youtube.com/watch?v=NmPQNvbXyic")
+
+};
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -77,32 +77,151 @@
 
     
 
+         $(".toWhite").on("click", function(){
+            $(this).toggleClass("white selected").text(" ")
+
+               if (($("#toWhite1").hasClass("white")) && ($("#toWhite2").hasClass("white"))){
+
+                $(".white").addClass("unwiped")
+                $(".white").delay(800).addClass("wiped") 
+            };     
+               return toCheck();
+
+        });
+
+         $(".toYellow").on("click", function(){
+            $(this).toggleClass("yellow selected").text(" ")
+            
+               if (($("#toYellow1").hasClass("yellow")) && ($("#toYellow2").hasClass("yellow"))){
+
+                $(".yellow").addClass("unwiped")
+                $(".yellow").delay(800).addClass("wiped") 
+            };  
+             return toCheck();
+        });
+
+
+         $(".toGrey").on("click", function(){
+            $(this).toggleClass("grey selected").text(" ")
+          
+               if (($("#toGrey1").hasClass("grey")) && ($("#toGrey2").hasClass("grey"))){
+
+                $(".grey").addClass("unwiped")
+                $(".grey").delay(800).addClass("wiped").addClass("disabled")
+            } ;
+             
+             return toCheck();
+
+        });
+
+         $(".toDark").on("click", function(){
+            $(this).toggleClass("dark selected").text(" ")
+            
+               if (($("#toDark1").hasClass("dark")) && ($("#toDark2").hasClass("dark"))){
+
+                $(".dark").addClass("unwiped")
+                 $(".dark").delay(800).addClass("wiped") 
+            };  
+             return toCheck();
+        });
+
+
+         $(".toBlack").on("click", function(){
+            $(this).toggleClass("black selected").text(" ")
+          
+               if (($("#toBlack1").hasClass("black")) && ($("#toBlack2").hasClass("black"))){
+
+                $(".black").addClass("unwiped")
+                $(".black").delay(800).addClass("wiped") 
+            } ;
+             
+             return toCheck();
+
+        });
+
+         $(".toPink").on("click", function(){
+            $(this).toggleClass("pink selected").text(" ")
+            
+               if (($("#toPink1").hasClass("pink")) && ($("#toPink2").hasClass("pink"))){
+
+                $(".pink").addClass("unwiped")
+                $(".pink").delay(800).addClass("wiped") 
+            };  
+             return toCheck();
+        });
+
+
+         $(".toLight").on("click", function(){
+            $(this).toggleClass("light selected").text(" ")
+          
+               if (($("#toLight1").hasClass("light")) && ($("#toLight2").hasClass("light"))){
+
+                $(".light").addClass("unwiped")
+                $(".light").delay(800).addClass("wiped") 
+            } ;
+             
+             return toCheck();
+
+        });
+
+           $(".toSmth").on("click", function(){
+            $(this).toggleClass("smth selected").text(" ")
+          
+               if (($("#toSmth1").hasClass("smth")) && ($("#toSmth2").hasClass("smth"))){
+
+                $(".smth").addClass("unwiped")
+                $(".smth").delay(800).addClass("wiped") 
+            } ;
+             
+             return toCheck();
+
+        });
+
+// First part of the toCheck function is supposed to check for us if we matched the pair of tiles, and "flip it" back if we didn't
+
          function toCheck(selected, wiped){
 
                 var selected = $(".selected").length;
-                var color = $(".red .blue .green .yellow .white .orange .grey ").length;
+                var color = $(".red .blue .green .yellow .white .orange .grey .dark .light .smth .black .pink ").length;
            
                  if((selected == 2)&&(color  != 2)) {
                     setTimeout(function(){
-                         $(".selected").removeClass("red blue green yellow white orange grey selected")
+                         $(".selected").removeClass("red blue green yellow white orange grey dark light smth black pink selected")
                     },400); 
                    
                 };
+//Once we paired all the tiles, we want this function to show us the button, that allow us to go to next level
+                 var wiped = $(".wiped").length;
+                 var location = $("#home")
 
-                 var wiped = $(".wiped").length
-
-                if(wiped == 9){
+               if(($("#home").hasClass("active"))&&(wiped == 9)){
                     setTimeout(function(){
-
                         $("#welcome").addClass("disabled")
                         $("#theGame").addClass("disabled")
                         $("#Complete").removeClass("disabled");
                     },700); 
-                    
-                } ;
-            }
-          
-
+                }else if ($("#level2").hasClass("active")&&(wiped == 15)){
+                     setTimeout(function(){
+                         
+                        $("#welcome").addClass("disabled")
+                        $("#theGame").addClass("disabled")
+                        $("#Complete").removeClass("disabled");
+                    },700)
+                }else if (($("#level3").hasClass("active"))&&(wiped == 24)){
+                    alert(`Your time was: `)
+                     setTimeout(function(){
+                        $("#welcome").addClass("disabled")
+                        $("#theGame").addClass("disabled")
+                        $("#Complete").removeClass("disabled");
+                    },700); 
+                }
+            };
+           
+// Check time function is the idea of my friend Jack, who was the first person to check if he likes my app.
+// While creating it, I was following the tutorial found on Youtube at: https://www.youtube.com/watch?v=knbUg0Q3qLU
         
  }) 
 
+
+
+     
